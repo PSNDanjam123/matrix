3c6a749334f2dbbf325b93ff0c0b294378f53a6f
Jamie Bailey <psndanjam123@gmail.com>
2018-03-07 12:28:08 +0000

    wrote demo

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,13 @@ int main(void) {
     Mat4<float> m4;
     Mat<int> m(2,3);
 
+    int t = 1;
+
     m.map([](float, unsigned x, unsigned y) {
             return x + y;
-            });
+            }).map([&t](float val, unsigned, unsigned) {
+                return val + t++;
+                });
 
     std::cout << m.str();
 
