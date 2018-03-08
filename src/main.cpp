b95cd18f484063f5fbf39d6a37549ba6b6689143
Jamie Bailey <psndanjam123@gmail.com>
2018-03-08 16:25:18 +0000

    translate added

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,11 @@
 using namespace Jim::Matrix;
 
 int main(void) {
-    Mat<int> m1(3,2);
-    Mat<int> m2(2,3);
-    Mat<int> m3(2,2);
+    Mat4f m;
 
-    m1(0,0) = 1;
-    m1(1,0) = 2;
-    m1(2,0) = 3;
-    m1(0,1) = 4;
-    m1(1,1) = 5;
-    m1(2,1) = 6;
+    m.translate(50, 20, 10) += m.identity();
 
-    m2(0,0) = 7;
-    m2(1,0) = 8;
-    m2(0,1) = 9;
-    m2(1,1) = 10;
-    m2(0,2) = 11;
-    m2(1,2) = 12;
-    m3 += m1 * m2;
-
-    std::cout << m1.str() << '\n' << m2.str() << '\n' << m3.str();
+    std::cout << m.str();
 
     return 0;
 }
