e151c727fe953d92b4cb506fbbe7de7b03d77510
Jamie Bailey <psndanjam123@gmail.com>
2018-03-09 09:52:40 +0000

    identity added

--- src/Jim/Core/Matrix.h
+++ src/Jim/Core/Matrix.h
@@ -1,5 +1,9 @@
 #pragma once
 #include <vector>
+#include <functional>
+#include <sstream>
+#include <string>
+
 
 namespace Jim::Core {
     template <typename T>
@@ -8,7 +12,7 @@ namespace Jim::Core {
                 Matrix(unsigned cols, unsigned rows) {
                     this->resize(cols, rows);
                 }
-                Matrix resize(unsigned& cols, unsigned& rows) { 
+                Matrix& resize(unsigned cols, unsigned rows) { 
                     this->_cols = cols;
                     this->_rows = rows;
                     this->_matrix.resize(this->_rows);
@@ -16,10 +20,52 @@ namespace Jim::Core {
                     for(r = 0; r < this->_rows; r++) {
                         this->_matrix[r].resize(this->_cols);
                     }
+                    return *this;
+                }
+                std::string str() {
+                    std::stringstream output;
+                    this->_forEach([this, &output](T val, unsigned& c, unsigned&) {
+                            output << val << '\t';
+                            if(c == this->_cols - 1) {
+                                output << '\n';
+                            }
+                            });
+                    return output.str();
+                }
+                Matrix& forEach(std::function<void (T val, unsigned& c, unsigned& r)> callback) {
+                    return this->_forEach(callback);
+                }
+                Matrix& map(std::function<T (T val, unsigned& c, unsigned& r)> callback) {
+                    return this->_map(callback);
+                }
+                Matrix& set(unsigned c, unsigned r, T val) {
+                    this->_matrix[r][c] = val;
+                    return *this;
+                }
+                T get(unsigned c, unsigned r) {
+                    return this->_matrix[r][c];
+                }
+                Matrix identity() {
+                    Matrix<T> identity(this->_cols, this->_rows);
+                    return identity.map([](T, unsigned& c, unsigned& r) {return (c == r) ? 1 : 0;});
                 }
             private:
                 std::vector<std::vector<T>> _matrix;
                 unsigned _cols;
                 unsigned _rows;
+                Matrix& _forEach(std::function<void (T val, unsigned& c, unsigned& r)> callback) {
+                    unsigned r, c;
+                    for(r = 0; r < this->_rows; r++) {
+                        for(c = 0; c < this->_cols; c++) {
+                            callback(this->_matrix[r][c], c, r);
+                        }
+                    }
+                    return *this;
+                }
+                Matrix& _map(std::function<T (T val, unsigned& c, unsigned& r)> callback) {
+                    return this->_forEach([this, &callback](T val, unsigned& c, unsigned& r) {
+                            this->set(c,r, callback(val, c, r));
+                            });
+                }
         };
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include "./Jim/Core/Matrix.h"
+#include <iostream>
 using namespace Jim::Core;
 
 int main(void) {
     Matrix<float> m(4,4);
+
+    m.resize(3,3).resize(2,2);
+
+    std::cout << m.identity().str();
 
     return 0;
 }
