779b75d304e30ebb53c8d8b732b34697fc2db41b
Jamie Bailey <psndanjam123@gmail.com>
2018-03-12 15:47:54 +0000

    rewrite

---
+++ src/Jim/Core/Matrix.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <vector>
+#include <functional>
+#include <sstream>
+
+namespace Jim::Core {
+    template <typename BT>  //Base Type
+        class Matrix {
+            public:
+                Matrix(unsigned rows, unsigned cols) {
+                    this->resize(cols, rows);
+                }
+                Matrix& resize(unsigned cols, unsigned rows) {
+                    this->_cols = cols;
+                    this->_rows = rows;
+                    this->_matrix.resize(this->_rows);
+                    unsigned r;
+                    for(r = 0; r < this->_rows; r++) {
+                        this->_matrix[r].resize(this->_cols);
+                    }
+                    return *this;
+                }
+                BT get(unsigned c, unsigned r) {
+                    return this->_matrix[r][c];
+                }
+                Matrix& set(unsigned c, unsigned r, BT val) {
+                    this->_matrix[r][c] = val;
+                    return *this;
+                }
+                std::string str() {
+                    std::stringstream output;
+                    this->forEach([this, &output](BT val, unsigned& c, unsigned&) {
+                            output << val << '\t';
+                            if(c == this->_cols - 1) {
+                            output << '\n';
+                            }
+                            });
+                    return output.str();
+                }
+                Matrix& forEach(std::function<void (BT val, unsigned& c, unsigned& r)> callback) {
+                    unsigned r, c;
+                    for(r = 0; r < this->_rows; r++) {
+                        for(c = 0; c < this->_cols; c++) {
+                            callback(this->_matrix[r][c], c, r);
+                        }
+                    }
+                    return *this;
+                }
+                Matrix& map(std::function<BT (BT val, unsigned& c, unsigned& r)> callback) {
+                    return this->forEach([this, &callback](BT val, unsigned& c, unsigned& r) {
+                            this->set(c, r, callback(val, c, r));
+                            });
+                }
+            private:
+                long unsigned _rows;
+                long unsigned _cols;
+                std::vector<std::vector<BT>> _matrix;
+        };
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include "./Jim/Core/Matrix.h"
+using namespace Jim::Core;
 
 int main(void) {
+    Matrix<int> m1(4,4);
+    Matrix<float> m2(1,4);
+
+    std::cout << m1.str();
+
     return 0;
 }
