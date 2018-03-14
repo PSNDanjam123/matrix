a34077095dd293a12a0b41096008e539f102f59a
Jamie Bailey <psndanjam123@gmail.com>
2018-03-14 16:29:41 +0000

    cleaned up controls

--- src/ship.cpp
+++ src/ship.cpp
@@ -105,7 +105,7 @@ void Ship::threadRender() {
                 info += "DAM:  \t";
             }
 
-            info += "\tCONROLS: move=w/a/s/d;rotate=q/e;dampener=i;";
+            info += "\tCONROLS: mov=w/a/s/d;rot=q/e;dam=i;";
 
             c = Ship::USS_Ent.object.matrix * Ship::USS_Ent.object.c;
             x = Ship::USS_Ent.object.matrix * Ship::USS_Ent.object.x;
