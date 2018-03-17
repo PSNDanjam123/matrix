60a4a6043bbf6d49ca6f5d64f6cfb3f26bf654e8
Jamie Bailey <psndanjam123@gmail.com>
2018-03-17 13:28:33 +0000

    fixed perspective

--- src/Jim/Component/Camera.cpp
+++ src/Jim/Component/Camera.cpp
@@ -6,17 +6,17 @@ using namespace Jim::Core::Math;
 using namespace std;
 
 Jim::Component::Camera::Camera() {
-    this->setFOV(.9);
+    this->setFOV(90);
 }
 
 unit Jim::Component::Camera::getFOV() {
     xyz viewerPos = this->_viewer.getPosition();
     unit z = viewerPos.z;
-    return radToDeg(2 * atan(1/z));
+    return 100 * radToDeg(2 * atan(1/z));
 }
 
 void Jim::Component::Camera::setFOV(unit fov) {
-    unit f = degToRad(fov);
+    unit f = degToRad(fov) / 100;
     unit z = 1 / tan(f/2);
     xyz viewerPos = this->_viewer.getPosition();
     this->_viewer.setPosition(viewerPos.x, viewerPos.y, z);
